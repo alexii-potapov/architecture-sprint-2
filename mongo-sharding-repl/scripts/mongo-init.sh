3b763e3d4b863ae13adc3217cdefd03d5c24c935
Потапов Алексей <potapov@anc.space>
2025-02-01 22:40:31 +0300

    Смена EOF файла

--- mongo-sharding-repl/scripts/mongo-init.sh
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -74,3 +74,4 @@ use somedb
 for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
 EOF
 
+
--- sharding-repl-cache/scripts/mongo-init.sh
+++ sharding-repl-cache/scripts/mongo-init.sh
@@ -74,3 +74,5 @@ use somedb
 for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
 EOF
 
+
+
